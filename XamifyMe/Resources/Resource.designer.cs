0b4cc1ef5ab5eef5695ba44a571ece8fa60052ac
Kate Catlin <kcatlin2@gmail.com>
2017-04-05 13:24:11 -0600

    Creating buttons

--- XamifyMe/Resources/Resource.designer.cs
+++ XamifyMe/Resources/Resource.designer.cs
@@ -44,6 +44,15 @@ namespace XamifyMe
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050003
+			public const int CallButton = 2131034115;
+			
+			// aapt resource value: 0x7f050001
+			public const int PhoneNumberText = 2131034113;
+			
+			// aapt resource value: 0x7f050002
+			public const int TranslateButton = 2131034114;
+			
 			// aapt resource value: 0x7f050000
 			public const int textView1 = 2131034112;
 			
