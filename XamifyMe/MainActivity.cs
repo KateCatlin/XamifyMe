37bfa9cac17d474e475c233c832acedc93573705
Kate Catlin <kcatlin2@gmail.com>
2017-04-05 13:48:44 -0600

    Fixing errors in naming

--- XamifyMe/MainActivity.cs
+++ XamifyMe/MainActivity.cs
@@ -1,4 +1,8 @@
-﻿using Android.App;
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
 using Android.Widget;
 using Android.OS;
 
@@ -7,20 +11,47 @@ namespace XamifyMe
 	[Activity(Label = "XamifyMe", MainLauncher = true, Icon = "@mipmap/icon")]
 	public class MainActivity : Activity
 	{
-		int count = 1;
-
-		protected override void OnCreate(Bundle savedInstanceState)
+		protected override void OnCreate(Bundle bundle)
 		{
-			base.OnCreate(savedInstanceState);
+			base.OnCreate(bundle);
 
-			// Set our view from the "main" layout resource
 			SetContentView(Resource.Layout.Main);
+			EditText phoneNumberText = FindViewById<EditText>(Resource.Id.PhoneNumberText);
+			Button translateButton = FindViewById<Button>(Resource.Id.TranslateButton);
+			Button callButton = FindViewById<Button>(Resource.Id.CallButton);
+			callButton.Enabled = false;
 
-			// Get our button from the layout resource,
-			// and attach an event to it
-			Button button = FindViewById<Button>(Resource.Id.myButton);
+			string translatedNumber = string.Empty;
+			translateButton.Click += (object sender, EventArgs e) => 
+				{
+					translatedNumber = Core.PhonewordTranslator.ToNumber(phoneNumberText.Text);
+					if (String.IsNullOrWhiteSpace(translatedNumber))
+					{
+						callButton.Text = "Call";
+						callButton.Enabled = false;
+					}
+					else
+					{
+						callButton.Text = "Call " + translatedNumber;
+						callButton.Enabled = true;
+					}
+				};
+		
+			callButton.Click += (object sender, EventArgs e) =>
+			{
+				var callDialog = new AlertDialog.Builder(this);
+				callDialog.SetMessage("Call " + translatedNumber + "?");
+				callDialog.SetNeutralButton("Call", delegate
+				{
+					var callIntent = new Intent(Intent.ActionCall);
+					callIntent.SetData(Android.Net.Uri.Parse("tel:" + translatedNumber));
+					StartActivity(callIntent);
+				});
 
-			button.Click += delegate { button.Text = $"{count++} clicks!"; };
+				callDialog.SetNegativeButton("Cancel", delegate { });
+				callDialog.Show();
+			};
+		
 		}
 	}
 }
